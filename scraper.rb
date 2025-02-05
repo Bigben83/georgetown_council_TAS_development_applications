b2df773777e676974752a221a93454e119785f57
Benjamin Harris <Bigben83@users.noreply.github.com>
2025-02-06 07:44:04 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -77,14 +77,14 @@ doc.css('div.row.py-4.map-address .card-body').each do |application|
   logger.info("Application Details: #{application_details}")
 
   # Extract specific fields
-  description = application_details[3]&.last # Proposal
-  address = application_details[2]&.last # Location
-  council_reference = application_details[0]&.last # Application ID
-  applicant = application_details[1]&.last # Applicant Name
-  title_reference = application_details[4]&.last # Title Reference
-  date_received = application_details[6]&.last # Opening Date
-  closing_date = application_details[7]&.last # Closing Date
-  document_description = application_details[8]&.last # Document link
+  description = application_details[3].last # Proposal
+  address = application_details[2].last # Location
+  council_reference = application_details[0].last # Application ID
+  applicant = application_details[1].last # Applicant Name
+  title_reference = application_details[4].last # Title Reference
+  date_received = application_details[6].last # Opening Date
+  closing_date = application_details[7].last # Closing Date
+  document_description = application_details[8].last # Document link
 
   # Log the extracted data for clarity
   logger.info("Extracted Data: #{description}, #{address}, #{council_reference}, #{applicant}, #{title_reference}, #{date_received}, #{closing_date}, #{document_description}")
